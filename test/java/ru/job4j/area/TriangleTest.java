544ffccc704255e60c5f7953d26ad8531bf8c0c5
Vitaly Berstenev <sm_plus82@mail.ru>
2017-07-18 13:41:41 +0400

    vitaly add test

---
+++ test/java/ru/job4j/area/TriangleTest.java
@@ -0,0 +1,26 @@
+package job4j.area;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+
+public class TriangleTest {
+    @Test
+    public void areaTriangle(){
+        Triangle.Point a = new Triangle.Point(0, 0);
+        Triangle.Point b = new Triangle.Point(0, 2);
+        Triangle.Point c = new Triangle.Point(2, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 2D;
+        assertThat(result, closeTo(expected),0.1);
+        /**
+         ????????????????????????????????
+         */
+        
+    }
+
+    private void closeTo(double expected, double v) {
+    }
+}
